9dd6357d95a75de71b489f89fa64e0d3ffbab26b
Astro <jpalato101@gmail.com>
2024-07-06 14:34:52 -0700

    made responiveness better in services page, made font more rresponsive in about page

--- services.js
+++ services.js
@@ -12,3 +12,17 @@ accordionButtons.forEach(accordionButton => {
         }
     });
 });
+
+
+function alterMaxHeight() {
+    const accordionButtons = document.querySelectorAll('.accordion-button');
+    accordionButtons.forEach(accordionButton => {
+        const accordionItemBody = accordionButton.parentElement.nextElementSibling;
+        if (accordionButton.classList.contains('active')) {
+            accordionItemBody.style.transition = 'none';
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
+            accordionItemBody.style.transition = 'max-height .3s ease-out';
+        } 
+    })
+}
+window.addEventListener('resize', alterMaxHeight);
